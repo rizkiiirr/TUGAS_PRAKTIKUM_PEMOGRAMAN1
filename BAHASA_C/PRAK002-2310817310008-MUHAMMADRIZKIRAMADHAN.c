53331ac328e7616a584f4bebe04851e6f74660f7
rizkiiirr <145253962+rizkiiirr@users.noreply.github.com>
2023-09-18 13:47:20 +0800

    Add files via upload

---
+++ BAHASA_C/PRAK002-2310817310008-MUHAMMADRIZKIRAMADHAN.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("Selamat Pagi, Muhammad Rizki Ramadhan\n");
+    printf("Selamat Siang, Muhammad Rizki Ramadhan\n");
+    printf("Selamat Malam, Muhammad Rizki Ramadhan\n");
+
+}
---
+++ BAHASA_C/PRAK004-2310817310008-MUHAMMADRIZKIRAMADHAN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+printf("###############################\n");
+printf("#                             #\n");
+printf("#   Muhammad Rizki Ramadhan   #\n");
+printf("#        2310817310008        #\n");
+printf("#                             #\n");
+printf("###############################\n");
+
+return 0;
+}  
